59f71d0fe8612383886abbfeab59b8dd8085bbb8
eversonevo <73645835+eversonevo@users.noreply.github.com>
2022-09-23 19:51:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const host = '18.229.118.246';
 const app = require('./app');
 
 const server = http.createServer(app);
+server.listen(port,host, ()=> {
+    console.log(`Server is running on http://${host}:${port}`);
+});
 
 console.log('Funcionando');
 console.log('port');
-
 server.listen(port);
